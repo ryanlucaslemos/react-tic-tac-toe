f9971084b9e4adba8d5862afd703a2554fa4e395
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-28 16:53:26 -0300

    cria uma variavel de ambiente

---
+++ src/env.js
@@ -0,0 +1,18 @@
+
+const ENV = {
+    'PLAYERS': {
+        'PLAYER_1': {
+            'CSS_CLASS': 'fas fa-times fa-2x',
+            'PLAY_VALUE': 1
+        },
+        'PLAYER_2': {
+            'CSS_CLASS': 'far fa-circle fa-2x',
+            'PLAY_VALUE': -1
+        },
+        'NONE': {
+            'PLAY_VALUE': 0
+        }
+    }
+};
+
+export default ENV;
