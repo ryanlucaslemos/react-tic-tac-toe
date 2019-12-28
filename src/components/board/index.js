ec88f74255aede2e7f96a20c27d201f841932bdd
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-28 16:54:34 -0300

    Refatora e inclui o serviço de verificação

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 import './styles.css';
 import Cell from '../cell'
+import ENV from '../../env'
+import { verifyWinner, verifyDraw } from '../../services/gameVerification';
+
 
 const Board = () => {
     const [playsMatrix, setPlaysMatrix] = useState([
@@ -10,43 +13,49 @@ const Board = () => {
         Array(3).fill(0)
     ]);
 
-    const renderizaLinha = (linha, index) => {
+    const [round, setRound] = useState(1);
+
+    const renderLine = (line, index) => {
         return (
             <div className='board-row' key={index}>
-                {linha.map(renderizaCelula(index))}
+                {line.map(renderCell(index))}
             </div>
         );
     }
 
-    const renderizaCelula = parentKey => (celula, index) => {
+    const renderCell = parentKey => (cellValue, index) => {
 
         const cellKey = `${parentKey} ${index}`;
 
-        const makeAPlay = () => {
-            let newPlayMatrix = playsMatrix.concat([]);
-
-            const [parentIndex, index] = cellKey.split(' ')
-            
-
-            newPlayMatrix[parentIndex][index] = newPlayMatrix[parentIndex][index] === 0 ? 1 : -1;
-            
-            setPlaysMatrix(newPlayMatrix);
-
-            //console.log(playsMatrix)
-        };
-
         return (
-            <Cell key={cellKey} cell={celula} callbackParent={makeAPlay} />
+            <Cell key={cellKey} cell={cellValue} callbackParent={makeAMove(cellKey)} />
         );
+    };
+
+
+    const makeAMove = cellKey => () => {
+
+        const [parentIndex, index] = cellKey.split(' ');
+
+        let newPlayMatrix = playsMatrix.concat([]);
+
+        if (newPlayMatrix[parentIndex][index] !== ENV.PLAYERS.NONE.PLAY_VALUE) return;
+
+        if (round % 2 !== 0) {
+            newPlayMatrix[parentIndex][index] = ENV.PLAYERS.PLAYER_2.PLAY_VALUE;
+        } else {
+            newPlayMatrix[parentIndex][index] = ENV.PLAYERS.PLAYER_1.PLAY_VALUE;
+        }
+
+        verifyWinner(newPlayMatrix);
+        setPlaysMatrix(newPlayMatrix);
+        setRound(round + 1)
     };
 
     return (
         <div className='container'>
             {
-                console.log(playsMatrix)
-            }
-            {
-                playsMatrix.map(renderizaLinha)
+                playsMatrix.map(renderLine)
             }
 
         </div>
