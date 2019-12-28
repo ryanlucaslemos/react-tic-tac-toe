048407dc9cd06a32a5975783c94cc977d59ac69a
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-27 21:30:03 -0300

    cria a página do jogo

---
+++ src/pages/game/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+import Board from '../../components/board';
+
+
+const Game = () => {
+    return (
+        <div className='container'>
+            <Board />
+        </div>
+    );
+}
+
+export default Game;
