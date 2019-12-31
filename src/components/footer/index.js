89f7eda700ad02e326ed9de69586d58e7c53248f
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-31 16:21:07 -0300

    Inclui o link do linkedin

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,8 +5,8 @@ import './styles.css'
 const Footer = () => {
     return (
         <footer>
-            <p>Desenvolvido por: Ryan Lemos</p>
-            <p>Disponível em: <a href='https://github.com/ryanlucaslemos/react-tic-tac-toe'><i className='fab fa-github fa-1x'></i> ryanlucaslemos/react-tic-tac-toe</a></p>
+            <p>Desenvolvido por: <a href='https://www.linkedin.com/in/ryanlucaslemos/' target='_blank' rel="noopener noreferrer">Ryan Lemos</a></p>
+            <p>Disponível em: <a href='https://github.com/ryanlucaslemos/react-tic-tac-toe' target='_blank' rel="noopener noreferrer"><i className='fab fa-github fa-1x'></i> ryanlucaslemos/react-tic-tac-toe</a></p>
         </footer>
     );
 }
