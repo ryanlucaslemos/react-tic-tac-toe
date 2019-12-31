6806a959830c3cd6bf37fb00beec8898c7faadc9
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-31 15:50:36 -0300

    Finaliza o footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import './styles.css'
+
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <p>Desenvolvido por: Ryan Lemos</p>
+            <p>Disponível em: <a href='https://github.com/ryanlucaslemos/react-tic-tac-toe'><i className='fab fa-github fa-1x'></i> ryanlucaslemos/react-tic-tac-toe</a></p>
+        </footer>
     );
 }
 
