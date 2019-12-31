d3be693e78fdd2517f20be29f8e9a4fd889d4e95
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-31 16:22:58 -0300

    Melhora o design do quadro de pontuação

--- src/components/score-board/index.js
+++ src/components/score-board/index.js
@@ -4,26 +4,25 @@ import './styles.css'
 import GAME_MODEL from '../../game.model';
 
 const ScoreBoard = ({ players, draws }) => {
-    const { PLAYERS: {PLAYER_1, PLAYER_2 }} = GAME_MODEL;
+    const { PLAYERS: { PLAYER_1, PLAYER_2 } } = GAME_MODEL;
     return (
-        <div className='score-board-row'>
-            <div className='score-board'>
-                <h4>{players.player1.name}</h4>
-                <p><b>Vitórias:</b> {players.player1.wins}</p>
-                <hr />
-                <i className={PLAYER_1.CSS_CLASS}></i>
+        <div>
+            <div className='score-board-row'>
+                <div className='score-board'>
+                    <h4>{players.player1.name}</h4>
+                    <p><b>Vitórias:</b> {players.player1.wins}</p>
+                    <hr />
+                    <i className={PLAYER_1.CSS_CLASS}></i>
+                </div>
+                <h4 className='draws-style'>Empates: {draws}</h4>
+                <div className='score-board'>
+                    <h4>{players.player2.name}</h4>
+                    <p><b>Vitórias:</b> {players.player2.wins}</p>
+                    <hr />
+                    <i className={PLAYER_2.CSS_CLASS}></i>
+                </div>
             </div>
-
-            <div className='score-board'>
-                <h4>Empates: </h4>
-                <p>{draws}</p>
-            </div>
-            <div className='score-board'>
-                <h4>{players.player2.name}</h4>
-                <p><b>Vitórias:</b> {players.player2.wins}</p>
-                <hr />
-                <i className={PLAYER_2.CSS_CLASS}></i>
-            </div>
+            
         </div>
     );
 }
