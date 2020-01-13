ad59899b2d4d38087dc65c3d34508b2837d5f277
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-01-13 11:21:25 -0300

    Inclui o reinicio do jogo ao header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './styles.css';
 
-const Header = () => (
-  <header>
+const Header = ({ startGame }) => (
+  <header onClick={() => startGame()} onKeyDown={startGame} role="button" tabIndex={0}>
     <i className="fab fa-react" />
     {' '}
 React Tic-tac-toe
@@ -11,5 +12,9 @@ React Tic-tac-toe
     <i className="fab fa-slack-hash" />
   </header>
 );
+
+Header.propTypes = {
+  startGame: PropTypes.func.isRequired,
+};
 
 export default Header;
